571c42447d3f4e831c0081581446fc4356b83da3
Darina Y <darina.yarkaeva@gmail.com>
2023-01-30 21:39:31 +0500

    done first lesson

---
+++ dart_lesson_1/lesson_1/dart_lesson_1.dart
@@ -0,0 +1,75 @@
+// import 'package:characters/characters.dart';
+
+void main(List<String> arguments) {
+//   String a = "a";
+//   int b = 54;
+//   bool l = true;
+//   double d = 4.5;
+
+//   var e = 4;
+
+//   a = "";
+
+//   final int g = 65;
+//   const w = 65;
+
+//   print(a);
+
+//   final h=4;
+//  const v = 5;
+
+//  print (a.runtimeType);
+
+//   var bf = 4+ 5 - 4 * 5 / 6 % 3;
+//   var c = 4-4;
+//   var g = 4* 4;
+//   var w = 4 / 4;
+//   var e = 5 % 3; //
+//   var r = d + c;
+
+//   print (d);
+
+//   bool s = d<=4;
+
+//   print (s);
+
+//   bool t = ((4 > 5) && (6==6)) || (7<= 9);
+//   print (t);
+
+//   var l = 'qwe' + 'asd' + 'zxc';
+//   print (l);
+
+//   var p = 'asdafe';
+//   var z = 'sdffds';
+//   var m = 'fgsfdfsdf';
+//   var k = '$p$z $m';
+
+//   print(k);
+
+//   var list = ['asd','asdda', 'asdadsad', 'adsada'];
+//   var kontan = StringBuffer ();
+//   list.forEach((item){
+//     kontan.write(item);
+//   });
+//   var fff = kontan.toString();
+//   print(fff);
+
+  // var hi = 'Hi 🇩🇰';
+  // print(hi);
+  // print('The end of the string: ${hi.substring(hi.length - 1)}');
+  // print('The last character: ${hi.characters.last}');
+
+  // var d = int.parse("4");
+  // print(d);
+
+  // var d = int.tryParse("dfsf");
+  // print(d);
+  // var a = "hello";
+  // var b = "";
+
+  // int? c = null;
+  // c = 5;
+
+  // var r = c + 5;
+  // print(r);
+}
