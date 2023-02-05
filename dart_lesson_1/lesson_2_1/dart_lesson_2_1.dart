035368e03dc295d2a2855b4989dfa81a33c6bb9e
Darina Y <darina.yarkaeva@gmail.com>
2023-02-05 19:31:52 +0500

    corrected comments

--- dart_lesson_1/lesson_2_1/dart_lesson_2_1.dart
+++ dart_lesson_1/lesson_2_1/dart_lesson_2_1.dart
@@ -1,8 +1,8 @@
-// аргументы функций
+// Параметры функций
 
 void main() {
-//   final result = sum(1,6, 10, 10);
-//   print (result);
+  final result = sum(1, 6, 10, 10);
+  print(result);
 
   final a = makeUser(surname: 'Иванов', name: 'Иван', age: 12);
   final b = makeOldUser('Bdfy', 'dsds', 45);
@@ -10,28 +10,30 @@ void main() {
   User(name: 'Ivan', surname: 'Ivanoe', age: 14);
 }
 
-// int sum (
-//   int a,
-//   int b, [
-//   int c = 0 ,
-//   int d = 90,
-//   ]){
-//   return a + b + c + d;
-// }
+int sum(
+  int a,
+  int b, [
+  int c = 0,
+  int d = 90,
+]) {
+  return a + b + c + d;
+}
 
-String makeUser(
-    {String name = '',
-    String surname = '',
-    int age = 0,
-    String passport = ''}) {
+String makeUser({
+  String name = '',
+  String surname = '',
+  int age = 0,
+  String passport = '',
+}) {
   return "$name $surname $age $passport";
 }
 
-String makeUser2(
-    {required String name,
-    required String surname,
-    required int age,
-    String passport = ''}) {
+String makeUser2({
+  required String name,
+  required String surname,
+  required int age,
+  String passport = '',
+}) {
   return "$name $surname $age $passport";
 }
 
@@ -49,19 +51,20 @@ String makeUser3(int a, int b,
 }
 
 class User {
+  User({
+    required this.name,
+    required this.surname,
+    required this.age,
+    this.passport = "",
+  });
+
   final String name;
   final String surname;
   final int age;
   final String passport;
-
-  User(
-      {required this.name,
-      required this.surname,
-      required this.age,
-      this.passport = ""});
 }
 
 // ?? if null
-// [] - необязательные аргументы, могут иметь значение по умолчанию
-// {} - необязательные именнованные аргументы. значение по дефолту
-// {required } - обязательные именнованные аргументы, не могут иметь значения по дефолту
+// [] - необязательные аргументы, должны иметь значение по умолчанию
+// {} - необязательные именнованные аргументы, должны иметь значение по умолчанию
+// {required } - обязательные именнованные аргументы, не могут иметь значения по умолчанию
